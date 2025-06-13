88d6c4d114eeb3f04d557b9c58eceb2bfd16d236
rohitsolanki01 <rohitsolanki0473@gmail.com>
2025-06-13 18:41:14 +0530

    initial commite

--- public/js/app.js
+++ public/js/app.js
@@ -21,12 +21,3 @@
       alert.close();
     }
   }, 3000); 
-
-  //loader 
-  window.addEventListener("load", function () {
-    const loader = document.getElementById("preloader");
-    loader.style.transition = "opacity 0.5s";
-    loader.style.opacity = "0";
-    setTimeout(() => loader.style.display = "none", 600);
-  });
-  
